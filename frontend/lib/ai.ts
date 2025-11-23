6d813cede2edad55d5685e793747fd601a9c4850
michaelwaves <michaelyu713705@gmail.com>
2025-11-22 22:01:01 -0800

    commit

--- frontend/lib/ai.ts
+++ frontend/lib/ai.ts
@@ -1,6 +1,31 @@
+"use server"
 import OpenAI from "openai";
+import { fal } from "@fal-ai/client";
 const client = new OpenAI();
 
+fal.config({
+    credentials: process.env.FAL_KEY,
+});
+
+
+async function generate_image_from_text(prompt: string) {
+    const result = await fal.subscribe("fal-ai/flux/dev", {
+        input: {
+            prompt
+        },
+    });
+    return result
+}
+
+async function generate_image_from_text_and_image(prompt: string, image_url: string) {
+    const result = await fal.subscribe("fal-ai/flux/dev/image-to-image", {
+        input: {
+            prompt,
+            image_url
+        },
+    });
+    return result
+}
 async function inference(prompt: string) {
     const response = await client.responses.create({
         model: "gpt-5-nano",
@@ -11,4 +36,4 @@ async function inference(prompt: string) {
     return response.output_text
 }
 
-export { inference }
+export { inference, generate_image_from_text, generate_image_from_text_and_image }
